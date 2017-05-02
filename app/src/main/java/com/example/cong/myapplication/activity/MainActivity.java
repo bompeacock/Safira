cbd31f8db5cca4466ce86af6499f8455e1d523d5
Nguyễn Văn Công <whoever.crybaby@gmail.com>
2017-05-03 02:30:43 +0700

    j

---
+++ app/src/main/java/com/example/cong/myapplication/activity/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.cong.myapplication.activity;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+import com.example.cong.myapplication.R;
+import com.example.cong.myapplication.adapter.FragmentSimple;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.tabLayout)
+    TabLayout tabLayout;
+
+    @BindView(R.id.viewPage)
+    ViewPager viewPager;
+
+//    @BindView(R.id.drawerlayout)
+//    FlowingDrawer mDrawer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        //set ToolBar
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+        // Get the ViewPager and set it's PagerAdapter so that it can display items
+        viewPager.setAdapter(new FragmentSimple(getSupportFragmentManager(),
+                MainActivity.this));
+
+        // Give the TabLayout the ViewPager
+        tabLayout.setupWithViewPager(viewPager);
+
+
+//
+//        mDrawer.setTouchMode(ElasticDrawer.TOUCH_MODE_BEZEL);
+//        mDrawer.setOnDrawerStateChangeListener(new ElasticDrawer.OnDrawerStateChangeListener() {
+//            @Override
+//            public void onDrawerStateChange(int oldState, int newState) {
+//                if (newState == ElasticDrawer.STATE_CLOSED) {
+//                    Log.i("MainActivity", "Drawer STATE_CLOSED");
+//                }
+//            }
+//
+//            @Override
+//            public void onDrawerSlide(float openRatio, int offsetPixels) {
+//                Log.i("MainActivity", "openRatio=" + openRatio + " ,offsetPixels=" + offsetPixels);
+//            }
+//        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+}
