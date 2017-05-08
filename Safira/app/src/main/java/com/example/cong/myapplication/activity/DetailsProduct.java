82391b87dbfa7622ad3f0dab25786bbccf689db8
Nguyễn Văn Công <whoever.crybaby@gmail.com>
2017-05-08 07:32:14 +0700

    e

---
+++ Safira/app/src/main/java/com/example/cong/myapplication/activity/DetailsProduct.java
@@ -0,0 +1,16 @@
+package com.example.cong.myapplication.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.cong.myapplication.R;
+
+public class DetailsProduct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_product);
+
+    }
+}
